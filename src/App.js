c22110a642084b75d2e899c24746687538717918
Sherri Mitchell <rsamitchell@att.net>
2019-10-29 22:55:39 -0400

    added router to app file, and created links to latest and search pages.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import { App } from 'react'; 
+import { BrowserRouter as Router,
+	Route,
+	NavLink
+	 }
 
-function App() {
+// router for links to app pages - latest and search
+export default function App() {
+	  render() {
+		  	return (
+			  	<Router>
+			  		<div>
+				  		<div>
+				  			<NavLink to="/" activeClassName="latest">
+								Latest
+							</NavLink>
+				  		</div>
+				  		<div>
+				  			<NavLink to="/search" activeClassName="search">
+								Search
+							</NavLink>
+				  		</div>
+			  			
+			  			<div>
+					  		 <Route path="/latest">
+					  		 	<Latest />
+					    	</Route>
+
+					    	<Route path="/search">
+					    		<Search />
+					    	</Route>
+					    </div>
+			  		</div>
+			  	</Router>
+		  );
+	}
+}
+
+function Latest() {
   return (
     <div>
-      <p>Hello World</p>
+      <h2>Latest</h2>
     </div>
   );
 }
 
-export default App;
+function Search() {
+  return (
+    <div>
+      <h2>Search</h2>
+    </div>
+  );
+}
+
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
